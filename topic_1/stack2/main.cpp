5ef7a5ab9a96b130b3ec37ffb534a0b24d58e172
raymondh2003@yahoo.com <raymondh2003@yahoo.com>
2023-02-28 12:47:13 -0800

    Stack Exercise Formating

--- topic_1/stack2/main.cpp
+++ topic_1/stack2/main.cpp
@@ -5,16 +5,17 @@ int main(int argc, char const *argv[])
 {
     Stack<int> stack;
 
-    for (size_t i = 0; i < 90; ++i) {
+    for (size_t i = 0; i < 90; ++i)
+    {
         stack.push(i);
     }
-    for (size_t i = 0; i < 90; ++i) {
+    for (size_t i = 0; i < 90; ++i)
+    {
         std::cout << stack.peek() << " ";
         stack.pop();
     }
 
-
     std::cout << std::endl;
-    
+
     return 0;
 }
--- topic_1/stack2/stack.h
+++ topic_1/stack2/stack.h
@@ -1,43 +1,47 @@
-#include<stdexcept>
+#include <stdexcept>
 #ifndef STACK_H
 #define STACK_H
 
-template<typename U>
+template <typename U>
 struct Node
 {
     U item;
-    Node<U>* next;
+    Node<U> *next;
 };
 
+template <typename T>
+class Stack
+{
 
-template<typename T> 
-class Stack {
+public:
+    ~Stack()
+    {
+        while (!empty())
+            pop();
+    }
 
-    public:
+    bool empty() { return (head == nullptr); }
+    void push(const T &item) { head = new Node<T>{item, head}; }
 
-        ~Stack() {
-            while(!empty()) pop();
-        }
-        
-        bool empty() {return (head == nullptr);}
-        void push(const T& item) { head = new Node<T>{item, head}; }
+    void pop()
+    {
+        if (empty())
+            throw std::runtime_error("operation on empty stack");
 
-        void pop() {
-            if(empty()) throw std::runtime_error("operation on empty stack");
+        Node<T> *tmp{head};
+        head = head->next;
+        delete tmp;
+    }
 
-            Node<T>* tmp{head};
-            head = head->next;
-            delete tmp;
-        }
+    T peek()
+    {
+        if (empty())
+            throw std::runtime_error("operation on empty stack");
+        return head->item;
+    }
 
-        T peek() {
-            if(empty()) throw std::runtime_error("operation on empty stack");
-            return head->item;
-        }
-
-    private:
-        Node<T>* head{nullptr};
-
+private:
+    Node<T> *head{nullptr};
 };
 
 #endif
