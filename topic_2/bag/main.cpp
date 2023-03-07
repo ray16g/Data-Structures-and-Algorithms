a50db426216bd5a27e2c3b4d653262bb5266afb6
raymondh2003@yahoo.com <raymondh2003@yahoo.com>
2023-03-07 12:42:24 -0800

    ArrayBag test case

--- topic_2/bag/main.cpp
+++ topic_2/bag/main.cpp
@@ -3,5 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
+    ArrayBag<int> ab(10);
+
+    for (size_t i = 0; i < 100; ++i)
+    {
+        ab.add(i);
+    }
+    
+    while(ab.size() > 0)
+    {
+        int item {ab.grab()};
+        std::cout << item << " ";
+        std::cout << ab.remove(item) << std::endl;
+    }
+
+
     return 0;
 }
