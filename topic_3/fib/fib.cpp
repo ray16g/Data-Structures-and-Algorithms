c04dd97f65ecb7fd39973eb7295db74d596450a5
raymondh2003@yahoo.com <raymondh2003@yahoo.com>
2023-03-16 10:59:10 -0700

    Fibonacci Iterative Implementation

--- topic_3/fib/fib.cpp
+++ topic_3/fib/fib.cpp
@@ -9,9 +9,21 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-uint64_t fib(uint32_t n) 
-{
-    if(n <= 1) return n;
+// uint64_t fib(uint32_t n) 
+// {
+//     if(n <= 1) return n;
 
-    return fib(n - 1) + fib(n - 2);
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+uint64_t fib(uint32_t n)
+{
+    uint32_t l{0};
+    uint32_t r{1};
+    uint32_t tmp;
+    int c{0};
+
+    for(; c < n; tmp = l, l += r, r = tmp, ++c);
+
+    return l;
 }
