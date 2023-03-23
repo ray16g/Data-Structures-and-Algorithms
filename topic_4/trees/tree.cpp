ab9563d09db745c7ef353379b25537442c4a720a
raymondh2003@yahoo.com <raymondh2003@yahoo.com>
2023-03-23 12:42:36 -0700

    Tree DFT Stack, Tree Exercise

--- topic_4/trees/tree.cpp
+++ topic_4/trees/tree.cpp
@@ -19,6 +19,8 @@ void printDft(BinaryNode<T>* root)
 
     printDft(root->leftChild);
     printDft(root->rightChild);
+
+    std::cout << std::endl;
 }
 
 template<typename T>
@@ -35,14 +37,27 @@ void printDft(BinaryNode<T>* root, int)
 
         if(tmp->rightChild != nullptr)
             stack.push(tmp->rightChild);
-            
+
         if(tmp->leftChild != nullptr)
             stack.push(tmp->leftChild);
 
         std::cout << tmp->item << " ";
-
     }
+    std::cout << std::endl;
+}
 
+size_t max(size_t left, size_t right)
+{
+    return (left > right) ? left : right;
+}
+
+template<typename T>
+size_t height(BinaryNode<T>* root)
+{
+    if(root == nullptr)
+        return 0;
+    
+    return 1 + max(height<T>(root->leftChild), height<T>(root->rightChild));
 }
 
 int main(int argc, char const *argv[])
@@ -57,8 +72,10 @@ int main(int argc, char const *argv[])
     root = A;
 
     printDft(root);
-    std::cout << std::endl;
     printDft(root, 0);
+
+    std::cout << height(root);
 
     return 0;
 }
+
