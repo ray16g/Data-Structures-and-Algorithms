4e014643cc250abfcaaceefd8ffd631f8783f282
raymondh2003@yahoo.com <raymondh2003@yahoo.com>
2023-04-04 12:27:05 -0700

    String hashcode + compression for hashmap exercise

--- topic_6/hash.cpp
+++ topic_6/hash.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
+#define ARRAY_SIZE 524287
+
 union b32
 {
     int i;
     float f;
 
 };
+
+int min(int l, int r)
+{
+    return (l < r ? l : r);
+}
+
+int hashcode(const std::string& k)
+{
+    int code{0};
+
+    for (size_t i = 0; i < min(15, k.size()); i++)
+        code = code * 10 + k[i];
+    
+
+    return code;
+}
 
 int hashcode(int k) { return k; }
 
@@ -24,10 +42,14 @@ int hashcode(double k)
     return hashcode(*reinterpret_cast<long long*>(&k));
 }
 
+int compress(int hc)
+{
+    return (hc & ~(1 << 31)) % ARRAY_SIZE;
+} 
 
 int main(int argc, char const *argv[])
 {
     // std::cout << hashcode(25ll) << std::endl;
-    std::cout << hashcode(25.0) << std::endl;
+    std::cout << hashcode("nbvnbvnbnbvjhgmmmmmmmmmmmmmmmm") << std::endl;
     return 0;
 }
