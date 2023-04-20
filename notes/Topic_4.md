7a45e8fb7d247fb36cda197c07dee001af82c787
ray16k <raymondh2003@yahoo.com>
2023-04-19 20:38:29 -0700

    Hashset test

--- notes/Topic_4.md
+++ notes/Topic_4.md
@@ -1,11 +1,10 @@
 # Topic 4
 
 ## Properties
-- The number of nodes in a full binary tree of height h is: n = 2^h - 1
+- The number of nodes in a full binary tree of height h is: 
+    - n = 2^h - 1
 - Height of a full or complete binary tree having n nodes:
     - h = ceil(log(n + 1))
-- Number of leaf nodes of a full tree with height h
-    - n_l = 2^h - 1
 - The number of leaf nodes in a complete binary tree having a number of nodes n
     - n_l = floor((n+1/2))
 - Number of non-leaf nodes in a complete binary tree having a number of nodes (n)
--- topic_6/hash_set/test_hashset.cpp
+++ topic_6/hash_set/test_hashset.cpp
@@ -6,9 +6,17 @@ int main(int argc, char const *argv[])
 {
     Set<int>* set{new HashSet<int>([](int i) { return i; } , Prober::LINEAR)};
 
+    equals<bool>(true, set->empty(), "Set is empty");
     set->add(5);
+    equals<bool>(false, set->empty(), "Set is not empty");
+    equals<int>(1, set->size(), "Set size is 1");
     equals<bool>(true, set->contains(5), "Added 5 to set, contains 5");
     equals<bool>(false, set->contains(6), "Contains 6");
-
+    set->add(10);
+    equals<bool>(true, set->contains(10), "Added 10 to set, contains 10");
+    equals<int>(2, set->size(), "Set size is 2");
+    set->remove(10);
+    equals<bool>(false, set->contains(10), "Removed 10 from set, does not contain 10");
+    equals<int>(1, set->size(), "Set size is 1");
     return 0;
 }
